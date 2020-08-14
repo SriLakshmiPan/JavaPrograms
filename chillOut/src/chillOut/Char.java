547a22943686e0ef83be4dd83835b0ee2db793d5
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-08-14 17:33:34 -0400

    conflicts2

--- chillOut/src/chillOut/Char.java
+++ chillOut/src/chillOut/Char.java
@@ -1,10 +1,19 @@
 package chillOut;
- class Char {
+
+class Char {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-char c='A';
-System.out.println(c);
-	}
+		char c = 'A';
+		System.out.println(c);
+		char ce = 'A';
+		System.out.println(c);
 
+		char cd = 'A';
+		System.out.println(c);
+
+		char cde = 'A';
+		System.out.println(c);
+
+	}
 }
